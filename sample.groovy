5cdfbdb10aa29b8d9207517a2275fa3d76095aa9
jayesh <jayeshkr73608@gmail.com>
2022-05-04 22:04:07 +0530

    k

---
+++ sample.groovy
@@ -0,0 +1,28 @@
+def call ( String grepo = 'a', String gbranch = 'a', String gitcred = 'a'  ) {
+
+pipeline {
+    environment {
+        gitRepo = "${grepo}"
+        gitBranch = "${gbranch}"
+        gitCred = "${gitcred}"
+    }
+
+    agent none
+
+    triggers {
+        pollSCM '* * * * * '
+    }
+
+    stages {
+        stage('POLL SCM') {
+            agent{label 'docker_slave'}
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCred", url: "$gitRepo"]]])
+            }
+        }
+
+    }
+
+}
+
+}
