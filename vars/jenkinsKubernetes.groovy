d76f0fb532cba783b6476e4b8d4f0d4dd8c7182f
jayesh <jayeshkr73608@gmail.com>
2022-07-11 09:33:22 +0530

    commented

--- vars/jenkinsKubernetes.groovy
+++ vars/jenkinsKubernetes.groovy
@@ -1,81 +1,81 @@
-def call ( String dockerCred = 'a', String dockerRepo = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a'  ) {
+// def call ( String dockerCred = 'a', String dockerRepo = 'a', String docTag = 'a', String grepo = 'a', String gbranch = 'a', String gitcred = 'a'  ) {
 
-pipeline {
-    environment {
-        dockerCredential = "${dockerCred}"
-        dockerRepository = "${dockerRepo}"
-        dockerTag = "${docTag}_$BUILD_NUMBER"
-        gitRepo = "${grepo}"
-        gitBranch = "${gbranch}"
-        gitCred = "${gitcred}"
-    }
+// pipeline {
+//     environment {
+//         dockerCredential = "${dockerCred}"
+//         dockerRepository = "${dockerRepo}"
+//         dockerTag = "${docTag}_$BUILD_NUMBER"
+//         gitRepo = "${grepo}"
+//         gitBranch = "${gbranch}"
+//         gitCred = "${gitcred}"
+//     }
 
-    agent none
+//     agent none
 
-    triggers {
-        pollSCM '* * * * * '
-    }
+//     triggers {
+//         pollSCM '* * * * * '
+//     }
 
-    options {
-    buildDiscarder(logRotator(numToKeepStr: '5', artifactNumToKeepStr: '5'))
-    }
+//     options {
+//     buildDiscarder(logRotator(numToKeepStr: '5', artifactNumToKeepStr: '5'))
+//     }
 
-    stages {
-        stage('POLL SCM') {
-            agent{label 'docker_slave'}
-            steps {
-                checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCred", url: "$gitRepo"]]])
-            }
-        }
+//     stages {
+//         stage('POLL SCM') {
+//             agent{label 'docker_slave'}
+//             steps {
+//                 checkout([$class: 'GitSCM', branches: [[name: "$gitBranch"]], extensions: [], userRemoteConfigs: [[credentialsId: "$gitCred", url: "$gitRepo"]]])
+//             }
+//         }
 
-        stage('SonarQube analysis') {
-            agent{label 'docker_slave'}
-            steps { 
-                script{
-                    //def sonarScanner = tool name: 'sonarqube', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-                    def scannerHome = tool 'sonarqube';
-                    withSonarQubeEnv('Sonar_Server') {
-                        sh "${tool("scannerHome")}/bin/sonar-scanner \
-                        -D sonar.projectKey=jenkins \
-                        -D sonar.projectName=jenkins \
-                        -D sonar.host.url=http://52.66.115.34:9000/ \
-                        -D sonar.sources=/var/lib/jenkins/workspace/$JOB_NAME"
-                    }
-                }
-            }
-        }
+//         stage('SonarQube analysis') {
+//             agent{label 'docker_slave'}
+//             steps { 
+//                 script{
+//                     //def sonarScanner = tool name: 'sonarqube', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+//                     def scannerHome = tool 'sonarqube';
+//                     withSonarQubeEnv('Sonar_Server') {
+//                         sh "${tool("scannerHome")}/bin/sonar-scanner \
+//                         -D sonar.projectKey=jenkins \
+//                         -D sonar.projectName=jenkins \
+//                         -D sonar.host.url=http://52.66.115.34:9000/ \
+//                         -D sonar.sources=/var/lib/jenkins/workspace/$JOB_NAME"
+//                     }
+//                 }
+//             }
+//         }
 
-        //stage('BUILD STAGE') {
-        //     agent{label 'docker_slave'}
-        //     steps {
-        //         script {
-        //             //dockerimage = docker.build("'$dockerRepository':$dockerTag")
-        //             dockerimage = dockerImage = docker.build dockerRepository + ":$dockerTag" 
+//         //stage('BUILD STAGE') {
+//         //     agent{label 'docker_slave'}
+//         //     steps {
+//         //         script {
+//         //             //dockerimage = docker.build("'$dockerRepository':$dockerTag")
+//         //             dockerimage = dockerImage = docker.build dockerRepository + ":$dockerTag" 
 
-        //         }
-        //     }
-        // }
+//         //         }
+//         //     }
+//         // }
 
-        // stage('PUSH TO DOCKER HUB') {
-        //     agent{label 'docker_slave'}
-        //     steps { 
-        //         script {
-        //             docker.withRegistry('', "$dockerCredential") {
-        //                 dockerImage.push()
-        //             }
-        //             sh 'docker image rmi jayesh313/k8s:"$dockerTag"'
-        //         }
-        //     }
-        // } 
+//         // stage('PUSH TO DOCKER HUB') {
+//         //     agent{label 'docker_slave'}
+//         //     steps { 
+//         //         script {
+//         //             docker.withRegistry('', "$dockerCredential") {
+//         //                 dockerImage.push()
+//         //             }
+//         //             sh 'docker image rmi jayesh313/k8s:"$dockerTag"'
+//         //         }
+//         //     }
+//         // } 
 
-        // stage ('Deploy') {
-        //     agent{label 'k8s_slave'}
-        //     steps {
-        //         sh 'kubectl set image deployment/webapp-deployment nodejs="$dockerRepository:$dockerTag" --record'
-        //     }
-        // }
-    }
+//         // stage ('Deploy') {
+//         //     agent{label 'k8s_slave'}
+//         //     steps {
+//         //         sh 'kubectl set image deployment/webapp-deployment nodejs="$dockerRepository:$dockerTag" --record'
+//         //     }
+//         // }
+//     }
 
-}
+// }
 
-}
+// }
